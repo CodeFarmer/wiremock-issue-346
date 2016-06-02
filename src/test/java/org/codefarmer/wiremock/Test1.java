41f41e01ec1bfa1be43b97668f9c9553593866a0
Joel Gluth <joel.gluth@ticketmaster.co.uk>
2016-06-02 14:17:36 +0100

    Added extra param for easier path variability. Still no luck.

--- src/test/java/org/codefarmer/wiremock/Test1.java
+++ src/test/java/org/codefarmer/wiremock/Test1.java
@@ -1,8 +1,9 @@
 package org.codefarmer.wiremock;
 
-import org.junit.ClassRule;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+
+import org.junit.Rule;
 import org.junit.Test;
-import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,41 +16,40 @@ import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 
-public class Test1
-{
+public class Test1 {
 
-  @ClassRule
-  public static WireMockClassRule wireMockRule = new WireMockClassRule(0);
+  @Rule
+  public WireMockRule wireMockRule = new WireMockRule(0);
 
-  public String stub500() {
+  public String stub200(String extraThing) {
 
-    String path = "/" + getClass().getName();
+    String path = "/" + getClass().getName() + '/' + extraThing;
 
     stubFor(get(urlEqualTo(path))
         .willReturn(aResponse().withStatus(200)
-            .withHeader("Content-Type", "application/json")
-		    .withBody("{ \"message\": \"phooey\", \"severity\": 3}")));
+                               .withHeader("Content-Type", "application/json")
+                               .withBody("{ \"message\": \"phooey\", \"severity\": 3, \"extraThing\": \"" + extraThing +  "\"}")));
 
     return "http://localhost:" + wireMockRule.port() + path;
-
   }
 
   @Test
   public void testThing() throws IOException, InterruptedException {
 
     try {
-      String url = stub500();
-      URLConnection conx = new URL(url).openConnection();
+
+      String url1 = stub200("1");
+      String url2 = stub200("2");
+      String url3 = stub200("3");
+
+      URLConnection conx = new URL(url1).openConnection();
       BufferedReader reader = new BufferedReader(new InputStreamReader((conx.getInputStream())));
       reader.readLine();
       reader.close();
+    } finally {
+      //	    while(true) {
+      //	      Thread.sleep(1000);
+      //    	}
     }
-    finally {
-//	    while(true) {
-//	      Thread.sleep(1000);
-//    	}
-    }
-
   }
-
 }
